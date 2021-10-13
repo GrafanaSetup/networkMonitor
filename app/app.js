05012981a7b57491f659929583ab0edb6aba6896
Markus Simonsen <33492046+Markussim@users.noreply.github.com>
2021-10-13 13:44:52 +0200

    Broken

--- app/app.js
+++ app/app.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 //Dependencies import
 const ping = require("ping")
 const speedTest = require("speedtest-net")
+const Influx = require('influx')
 
 //Local Dependencies
 const database = require("./database.js");
@@ -13,23 +14,59 @@ const Speedtest = require("./models/Speedtest.js");
 //Variable Initialize
 const mongoURL = process.env.MONGOURL || "mongodb://localhost:27017/";
 
-console.log(mongoURL)
+//Influxdb init
+const influx = new Influx.InfluxDB({
+
+    host: 'localhost',
+    database: 'network_monitor',
+    username: 'test_username',
+    password: 'test_password',
+    schema: [
+      {
+        measurement: 'ping',
+        fields: { pingTime: Influx.FieldType.FLOAT },
+        tags: ['unit']
+      }
+    ]
+  });
+
+  influx.getDatabaseNames()
+  .then(names => {
+    if (!names.includes('network_monitor')) {
+      return influx.createDatabase('network_monitor');
+    }
+  })
+  .catch(error => console.log({ error }));
 
 //Connect to Mongo
-database.connect("GrafanaPing", mongoURL);
+//database.connect("GrafanaPing", mongoURL);
 
 let speedTestConfig = {
     acceptLicense: true,
     acceptGdpr: true
 }
 
-setInterval(newSpeedTest, 1800000)
+//setInterval(newSpeedTest, 1800000)
 
 setInterval(async () => {
     let pingVar = await ping.promise.probe("speedtest.net")
     console.log(pingVar.time);
-    let pingModel = await createPingModel(pingVar.time);
-    database.saveToDB(pingModel)
+    influx.writePoints([
+        {
+          measurement: 'ping',
+          tags: {
+            unit: "ms",
+          },
+          fields: { pingTime: pingVar.time },
+          timestamp: Date.now(),
+        }
+      ], {
+        database: 'network_monitor',
+        precision: 'n',
+      })
+      .catch(error => {
+        console.error(`Error saving data to InfluxDB! ${err.stack}`)
+      });
 }, 2000)
 
 async function newSpeedTest() {
