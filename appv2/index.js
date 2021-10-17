24f82c4e36b38106c15e9ea603b6f75b07eb773a
Markus Simonsen <33492046+Markussim@users.noreply.github.com>
2021-10-17 19:13:09 +0200

    Added bandwidth

--- appv2/index.js
+++ appv2/index.js
@@ -1,17 +1,49 @@
 const express = require('express')
 const ping = require("ping")
+const speedTest = require("speedtest-net")
 const app = express()
 const port = 7896
-app.get('/metrics', async (req, res) => {
+
+let speedTestConfig = {
+        acceptLicense: true,
+        acceptGdpr: true
+}
+
+app.get('/ping/metrics', async (req, res) => {
         let pingVar = await ping.promise.probe("speedtest.net")
         console.log(pingVar)
         if (pingVar.time > 0.1) {
-                res.send(`# HELP go_ping_time_ms Current ping time \n# TYPE go_ping_time_ms gauge\ngo_ping_time_ms ${pingVar.time}`)
+                res.send(`
+                # HELP go_ping_time_ms Current ping time
+                # TYPE go_ping_time_ms gauge'
+                go_ping_time_ms ${pingVar.time}
+                `)
         } else {
                 res.status(503).send("No connection to internet")
         }
 
 });
+
+app.get('/bandwidth/metrics', async (req, res) => {
+        console.log("Started speedtest")
+        let success = true
+        let testResult = await speedTest(speedTestConfig).catch(error => success = false)
+
+        if (testResult.download != undefined) {
+                console.log(testResult.download.bandwidth)
+                res.send(`
+                # HELP go_bandwidth_upload Current bandwidth up in bytes per second
+                # TYPE go_bandwidth_upload gauge'
+                go_bandwidth_upload ${testResult.upload.bandwidth}
+                # HELP go_bandwidth_download Current bandwidth down in bytes per second
+                # TYPE go_bandwidth_download gauge'
+                go_bandwidth_download ${testResult.download.bandwidth}
+                `)
+        } else {
+                res.status(503).send("No connection to internet")
+        }
+});
+
 
 app.listen(port, () => {
         console.log("pucko")
